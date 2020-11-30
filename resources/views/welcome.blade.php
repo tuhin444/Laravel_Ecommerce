b3bdacf2a5c6de2f3244241493edd7f1c23f858f
tuhin444 <tuhincse444@gmail.com>
2020-11-30 23:17:33 +0600

    commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -60,6 +60,7 @@
 
             .m-b-md {
                 margin-bottom: 30px;
+				
             }
         </style>
     </head>
@@ -87,6 +88,7 @@
                 <div class="links">
                     <a href="https://laravel.com/docs">Docs</a>
                     <a href="https://laracasts.com">Laracasts</a>
+					
                     <a href="https://laravel-news.com">News</a>
                     <a href="https://blog.laravel.com">Blog</a>
                     <a href="https://nova.laravel.com">Nova</a>
